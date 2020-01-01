6c0990595f827575e0215777243f79b41eaa1f47
Akshay S Dinesh <asdofindia@gmail.com>
2020-01-01 15:57:37 +0530

    content: team in about

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,8 +7,17 @@ import SEO from "../components/seo"
 const AboutPage = () => (
   <Layout>
     <SEO title="About" />
-    <h1>AFPI Karnataka Newsletter</h1>
-    <p>Being taken online</p>
+    <h1>About</h1>
+    <h2>Team</h2>
+    <ul>
+      <li>Akshay S Dinesh</li>
+      <li>BC Rao</li>
+      <li>Ramakrishna Prasad</li>
+      <li>Roshni Jhan Ganguly</li>
+      <li>Sowmya B. Ramesh</li>
+      <li>Sulaiman Sharieff</li>
+      <li>Swathi S Balachandra</li>
+    </ul>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
