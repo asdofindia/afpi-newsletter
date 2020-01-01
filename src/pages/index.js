4a55ed914b46cc4778468bcbeaa00efee07b1241
Akshay S Dinesh <asdofindia@gmail.com>
2020-01-01 17:26:39 +0530

    content: mission on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <p>The Newsletter is now available online</p>
+    <p>The AFPI Karnataka Newsletter is a quarterly newsletter published by AFPI</p>
+    <p>It is a semi-formal space where family physicians, general practitioners, and others interested in the field of primary health care can creatively share their experiences and express their opinions of what family medicine and primary health care should be, including their case reports, research papers, management, leadership, and relationship issues, administrative and entrepreneurial decisions, community work, and other articles about changing trends, thereby creating a community of learners and practitioners who inspire each other through their work and enhance and update their knowledge</p>
     <p><Link to="/volume-4/issue-1/">Latest Issue</Link></p>
     <p><Link to="/issues/">View all issues</Link></p>
     <p><Link to="/about/">About</Link></p>
