cb10211eef9e3e5a5798130583b68cfdc768c461
Akshay S Dinesh <asdofindia@gmail.com>
2020-01-01 15:57:24 +0530

    fix: link to old issues

--- src/pages/issues.js
+++ src/pages/issues.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import { OutboundLink } from "gatsby-plugin-google-analytics"
 import path from "path"
 
 import Layout from "../components/layout"
@@ -13,6 +14,18 @@ const getIssueIdFromPath = (absolutePath) => {
 }
 
 const IssuesPage = ({data}) => {
+  const olderIssues = [
+    'volume-1/issue-1',
+    'volume-1/issue-2',
+    'volume-1/issue-3',
+    'volume-1/issue-4',
+    'volume-2/issue-1',
+    'volume-2/issue-2',
+    'volume-3/issue-1',
+    'volume-3/issue-2',
+    'volume-3/issue-3',
+    'volume-3/issue-4'
+  ]
   const issuePaths = data.issueGroup.group
   const issues = issuePaths.map(({fieldValue}) => getIssueIdFromPath(fieldValue) )
   return (
@@ -21,7 +34,10 @@ const IssuesPage = ({data}) => {
       <ul>
         {issues.map((issue) => <li><Link to={`/${issue}/`}>{getIssueName(issue)}</Link></li>)}
       </ul>
-      <p>Older issues will soon be uploaded here</p>
+      <p>Older issues (published as PDF)</p>
+      <ul>
+        {olderIssues.reverse().map(issue => <li> <OutboundLink href={`/${issue}/issue.pdf`}> {getIssueName(issue)} (PDF)</OutboundLink></li>)}
+      </ul>
     </Layout>
   )
 }
