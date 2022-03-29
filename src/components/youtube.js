29c6f905847bb391c3af746364a6027783ef8d87
Akshay S Dinesh <akshay@learnlearn.in>
2022-03-29 20:41:39 +0530

    fix a11y warning

--- src/components/youtube.js
+++ src/components/youtube.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Youtube = ({id}) => (
-    <iframe width="560" height="315" src={`https://www.youtube.com/embed/${id}`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe title="video embed from youtube" width="560" height="315" src={`https://www.youtube.com/embed/${id}`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 )
 
 export default Youtube
